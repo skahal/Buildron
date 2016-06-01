462c776f224dc821d7f099607d3c96f4f116b639
Diego Giacomelli <giacomelli@gmail.com>
2016-05-31 23:28:56 -0300

    Added PostProcessBuild to copy mods files to built app.

---
+++ src/Buildron/Assets/Editor/BuildProcessor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.Callbacks;
+using UnityEditor;
+using System.IO;
+using System;
+
+public class BuildProcessor {
+
+	[PostProcessBuildAttribute(1)]
+	public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject) 
+	{
+		CopyModsFilesToBuild (pathToBuiltProject);
+	}
+
+	private static void CopyModsFilesToBuild(string pathToBuiltProject)
+	{
+		Debug.Log("BuildProcessor.OnPostprocessBuild");
+		Debug.LogFormat("pathToBuiltProject: {0}", pathToBuiltProject);
+		Debug.LogFormat("current directory: {0}", Application.dataPath);
+
+		var srcModsPath = Application.dataPath.Replace ("/Assets", "/mods");
+		Debug.LogFormat("srcModsPath: {0}", srcModsPath);
+
+		var destModsPath = pathToBuiltProject + "/mods";
+		Debug.LogFormat("destModsPath: {0}", destModsPath);
+
+		Directory.CreateDirectory(destModsPath);
+		var files = Directory.GetFiles (srcModsPath, "*.*", SearchOption.AllDirectories);
+
+		foreach (var file in files) {
+			var destFileName = destModsPath + file.Replace(srcModsPath, "");
+			Debug.LogFormat("Copying file: {0}", destFileName);
+
+			var destDir = Path.GetDirectoryName (destFileName);
+
+			if (!Directory.Exists (destDir)) {
+				Directory.CreateDirectory (destDir);
+			}
+
+			File.Copy (file, destFileName, true);
+		}
+	}
+}
