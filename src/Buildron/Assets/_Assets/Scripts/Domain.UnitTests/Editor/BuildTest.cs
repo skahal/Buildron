dc04b84fab886f2aa09c181cb44568909a35744d
Diego Giacomelli <giacomelli@gmail.com>
2016-06-21 21:10:00 -0300

    Improving Build code coverage.

---
+++ src/Buildron/Assets/_Assets/Scripts/Domain.UnitTests/Editor/BuildTest.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using Buildron.Domain;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Skahal.Logging;
+
+namespace Buildron.Domain.UnitTests
+{
+    [Category("Buildron.Domain")]
+    public class BuildTest
+    {
+        [TearDown]
+        public void TearDonw()
+        {
+            Build.EventInterceptors.Clear();
+        }
+
+        [Test]
+        public void Status_Changed_StatusChangedRaised()
+        {
+            var target = new Build();
+            var statusChangedRaised = target.CreateAssert<BuildStatusChangedEventArgs>("StatusChanged", 2);
+
+            Assert.AreEqual(BuildStatus.Unknown, target.Status);
+            Assert.AreEqual(BuildStatus.Unknown, target.PreviousStatus);
+
+            target.Status = BuildStatus.Running;
+            Assert.AreEqual(BuildStatus.Running, target.Status);
+            Assert.AreEqual(BuildStatus.Unknown, target.PreviousStatus);
+
+            target.Status = BuildStatus.Success;
+            Assert.AreEqual(BuildStatus.Success, target.Status);
+            Assert.AreEqual(BuildStatus.Running, target.PreviousStatus);
+
+            statusChangedRaised.Assert();
+        }
+
+        [Test]
+        public void Status_ChangedInterceptorCancelEvent_StatusChangedNotRaised()
+        {
+            var target = new Build();
+            var statusChangedRaised = target.CreateAssert<BuildStatusChangedEventArgs>("StatusChanged", 4);
+
+            var interceptor = MockRepository.GenerateMock<IBuildEventInterceptor>();
+            interceptor.Expect(i => i.OnStatusChanged(null)).IgnoreArguments().WhenCalled((m) =>
+            {
+                var buildEvent = m.Arguments[0] as BuildEvent;
+
+                buildEvent.Canceled = buildEvent.Build.Status == BuildStatus.Error;
+
+            });
+
+            Build.EventInterceptors.Add(interceptor);
+
+            target.Status = BuildStatus.Running;                       
+            target.Status = BuildStatus.Success;
+            target.Status = BuildStatus.Error;
+            target.Status = BuildStatus.Success;
+            target.Status = BuildStatus.Error;
+            target.Status = BuildStatus.Failed;
+
+            statusChangedRaised.Assert();
+        }
+
+        [Test]
+        public void IsProperties_DiffStatus_DiffIsResult()
+        {
+            var target = new Build();
+            Assert.IsFalse(target.IsFailed);
+            Assert.IsFalse(target.IsQueued);
+            Assert.IsFalse(target.IsRunning);
+            Assert.IsFalse(target.IsSuccess);
+
+            target.Status = BuildStatus.Canceled;
+            Assert.IsTrue(target.IsFailed);
+            Assert.IsFalse(target.IsQueued);
+            Assert.IsFalse(target.IsRunning);
+            Assert.IsFalse(target.IsSuccess);
+
+            target.Status = BuildStatus.Error;
+            Assert.IsTrue(target.IsFailed);
+            Assert.IsFalse(target.IsQueued);
+            Assert.IsFalse(target.IsRunning);
+            Assert.IsFalse(target.IsSuccess);
+
+            target.Status = BuildStatus.Failed;
+            Assert.IsTrue(target.IsFailed);
+            Assert.IsFalse(target.IsQueued);
+            Assert.IsFalse(target.IsRunning);
+            Assert.IsFalse(target.IsSuccess);
+
+            target.Status = BuildStatus.Queued;
+            Assert.IsFalse(target.IsFailed);
+            Assert.IsTrue(target.IsQueued);
+            Assert.IsFalse(target.IsRunning);
+            Assert.IsFalse(target.IsSuccess);
+
+            target.Status = BuildStatus.Running;
+            Assert.IsFalse(target.IsFailed);
+            Assert.IsFalse(target.IsQueued);
+            Assert.IsTrue(target.IsRunning);
+            Assert.IsFalse(target.IsSuccess);
+
+            target.Status = BuildStatus.RunningCodeAnalysis;
+            Assert.IsFalse(target.IsFailed);
+            Assert.IsFalse(target.IsQueued);
+            Assert.IsTrue(target.IsRunning);
+            Assert.IsFalse(target.IsSuccess);
+
+            target.Status = BuildStatus.RunningDeploy;
+            Assert.IsFalse(target.IsFailed);
+            Assert.IsFalse(target.IsQueued);
+            Assert.IsTrue(target.IsRunning);
+            Assert.IsFalse(target.IsSuccess);
+
+            target.Status = BuildStatus.RunningDuplicatesFinder;
+            Assert.IsFalse(target.IsFailed);
+            Assert.IsFalse(target.IsQueued);
+            Assert.IsTrue(target.IsRunning);
+            Assert.IsFalse(target.IsSuccess);
+
+            target.Status = BuildStatus.RunningFunctionalTests;
+            Assert.IsFalse(target.IsFailed);
+            Assert.IsFalse(target.IsQueued);
+            Assert.IsTrue(target.IsRunning);
+            Assert.IsFalse(target.IsSuccess);
+
+            target.Status = BuildStatus.RunningUnitTests;
+            Assert.IsFalse(target.IsFailed);
+            Assert.IsFalse(target.IsQueued);
+            Assert.IsTrue(target.IsRunning);
+            Assert.IsFalse(target.IsSuccess);
+
+            target.Status = BuildStatus.Success;
+            Assert.IsFalse(target.IsFailed);
+            Assert.IsFalse(target.IsQueued);
+            Assert.IsFalse(target.IsRunning);
+            Assert.IsTrue(target.IsSuccess);
+        }
+
+        [Test]
+        public void TriggeredBy_Changed_TriggeredByChangedRaised()
+        {
+            var target = new Build();
+            var changedRaised = target.CreateAssert<BuildTriggeredByChangedEventArgs>("TriggeredByChanged", 3);
+
+            Assert.IsNull(target.TriggeredBy);
+
+            target.TriggeredBy = new User { UserName = "u1" };
+            target.TriggeredBy = new User { UserName = "u1" };
+            target.TriggeredBy = new User { UserName = "u2" };
+            target.TriggeredBy = new User { UserName = "u2" };
+            target.TriggeredBy = new User { UserName = "u1" };
+
+            changedRaised.Assert();
+        }
+
+        [Test]
+        public void TriggeredBy_ChangedInterceptorCancelEvent_TriggeredByChangedNotRaised()
+        {
+            var target = new Build();
+            var changedRaised = target.CreateAssert<BuildTriggeredByChangedEventArgs>("TriggeredByChanged", 4);
+
+            var interceptor = MockRepository.GenerateMock<IBuildEventInterceptor>();
+            interceptor.Expect(i => i.OnTriggeredByChanged(null)).IgnoreArguments().WhenCalled((m) =>
+            {
+                var buildEvent = m.Arguments[0] as BuildEvent;
+
+                buildEvent.Canceled = buildEvent.Build.TriggeredBy.UserName.Equals("u3");
+
+            });
+
+            Build.EventInterceptors.Add(interceptor);
+
+            target.TriggeredBy = new User { UserName = "u1" };
+            target.TriggeredBy = new User { UserName = "u1" };
+            target.TriggeredBy = new User { UserName = "u2" };
+            target.TriggeredBy = new User { UserName = "u3" };
+            target.TriggeredBy = new User { UserName = "u2" };
+            target.TriggeredBy = new User { UserName = "u1" };
+            target.TriggeredBy = new User { UserName = "u3" };
+
+            changedRaised.Assert();
+        }
+
+        [Test]
+        public void ToString_ProjectAndConfig_String()
+        {
+            var target = new Build();
+            Assert.AreEqual(" - ", target.ToString());
+
+            target.Configuration.Project.Name = "Project";
+            target.Configuration.Name = "Config";
+
+            Assert.AreEqual("Project - Config", target.ToString());
+        }
+
+        [Test]
+        public void CompareTo_Builds_Sort()
+        {
+            var build1 = new Build { Configuration = new BuildConfiguration { Name = "C1", Project = new BuildProject { Name = "P1" } } };
+            Build build2 = null;
+            var build3 = new Build { Configuration = new BuildConfiguration { Name = "C0", Project = new BuildProject { Name = "P1" } } };
+
+            var builds = new Build[] { build1, build2, build3 };
+            Array.Sort(builds);
+
+            Assert.AreEqual(build2, builds[0]);
+            Assert.AreEqual(build3, builds[1]);
+            Assert.AreEqual(build1, builds[2]);
+        }
+
+        [Test]
+        public void Clone_NoArgs_NewInstance()
+        {
+            var target = new Build();
+            target.Configuration.Name = "C1";
+            target.Configuration.Project.Name = "P1";
+
+            var actual = target.Clone() as Build;
+            Assert.AreNotSame(target, actual);
+            Assert.AreEqual("C1", actual.Configuration.Name);
+            Assert.AreEqual("P1", actual.Configuration.Project.Name);
+        }
+    }
+
+}
--- src/Buildron/Assets/_Assets/Scripts/Domain/Build.cs
+++ src/Buildron/Assets/_Assets/Scripts/Domain/Build.cs
@@ -1,60 +1,121 @@
-#region Usings
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
-#endregion
 
 namespace Buildron.Domain
 {
-	#region Enums
-	public enum BuildStatus
-	{
+    #region Enums    
+    /// <summary>
+    /// Build status.
+    /// </summary>
+    public enum BuildStatus
+    {
+        /// <summary>
+        /// Build is in an unknown status.
+        /// </summary>
         Unknown,
-		Error,
-		Failed,
-		Canceled,
-		Success,
-		Queued,
-		Running,
+
+        /// <summary>
+        /// Build is in error status.
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// Build has failed
+        /// </summary>
+        Failed,
+
+        /// <summary>
+        /// Build has been canceled.
+        /// </summary>
+        Canceled,
+
+        /// <summary>
+        /// Build has finish with success.
+        /// </summary>
+        Success,
+
+        /// <summary>
+        /// Build has been queued.
+        /// </summary>
+        Queued,
+
+        /// <summary>
+        /// Build is running.
+        /// </summary>
+        Running,
+
+        /// <summary>
+        /// Build is running unit tests.
+        /// </summary>
 		RunningUnitTests,
+
+        /// <summary>
+        /// Build is running functional tests.
+        /// </summary>
 		RunningFunctionalTests,
+
+        /// <summary>
+        /// Build is running duplicates finder.
+        /// </summary>
 		RunningDuplicatesFinder,
+
+        /// <summary>
+        /// Build is running code analysis.
+        /// </summary>
 		RunningCodeAnalysis,
+
+        /// <summary>
+        /// Build is running deploy.
+        /// </summary>
 		RunningDeploy
-	}
-	#endregion
-	
-	public sealed class Build : IComparable<Build>, ICloneable
-	{
-		#region Constants
-		private const int SecondsToLockQueueStatus = 20;
-		#endregion
-		
-		#region Events
-		public event EventHandler<BuildStatusChangedEventArgs> StatusChanged;
-		public event EventHandler<BuildTriggeredByChangedEventArgs> TriggeredByChanged;
-		#endregion
-	
-		#region Fields
-		private BuildStatus m_status;
-		private User m_triggeredBy;
-		private static int s_instancesCount;
-		private DateTime m_lockCurrentStatusUntil = DateTime.Now;        
-		#endregion
-		
-		#region Constructors
+    }
+    #endregion
+
+    /// <summary>
+    /// Represents a build in continuous integration server.
+    /// </summary>
+    [DebuggerDisplay("{Configuration.Project.Name} - {Configuration.Name}: {Status}")]
+    public sealed class Build : IComparable<Build>, ICloneable
+    {
+        #region Constants        
+        /// <summary>
+        /// The seconds to lock queue status
+        /// </summary>
+        private const int SecondsToLockQueueStatus = 20;
+        #endregion
+
+        #region Events        
+        /// <summary>
+        /// Occurs when status has changed.
+        /// </summary>
+        public event EventHandler<BuildStatusChangedEventArgs> StatusChanged;
+
+        /// <summary>
+        /// Occurs when triggered by has changed.
+        /// </summary>
+        public event EventHandler<BuildTriggeredByChangedEventArgs> TriggeredByChanged;
+        #endregion
+
+        #region Fields
+        private BuildStatus m_status;
+        private User m_triggeredBy;
+        private static int s_instancesCount;
+        private DateTime m_lockCurrentStatusUntil = DateTime.Now;
+        #endregion
+
+        #region Constructors
         static Build()
         {
             EventInterceptors = new List<IBuildEventInterceptor>();
         }
 
-		public Build ()
-		{
+        public Build()
+        {
             Status = BuildStatus.Unknown;
-			Configuration = new BuildConfiguration ();
-			Sequence = ++s_instancesCount;
-		}
+            Configuration = new BuildConfiguration();
+            Sequence = ++s_instancesCount;
+        }
         #endregion
 
         #region Properties
@@ -64,149 +125,225 @@ namespace Buildron.Domain
         public static IList<IBuildEventInterceptor> EventInterceptors { get; private set; }
 
         public string Id { get; set; }
-		
-		public int Sequence { get; set; }
 
-		public BuildConfiguration Configuration { get; set; }
+        public int Sequence { get; set; }
 
-		public BuildStatus Status {
-			get {
-				return m_status;
-			}
-		
-			set {
-				if (m_status != value && DateTime.Now >= m_lockCurrentStatusUntil) {
-					
-					//if (value == BuildStatus.Running && m_status > BuildStatus.Running) {
-					//	return;
-					//} 
-					
-					if (value == BuildStatus.Queued) {
-						m_lockCurrentStatusUntil = DateTime.Now.AddSeconds(SecondsToLockQueueStatus);
-					}
+        public BuildConfiguration Configuration { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status.
+        /// </summary>
+        public BuildStatus Status
+        {
+            get
+            {
+                return m_status;
+            }
+
+            set
+            {
+                if (m_status != value && DateTime.Now >= m_lockCurrentStatusUntil)
+                {
+
+                    //if (value == BuildStatus.Queued)
+                    //{
+                    //    m_lockCurrentStatusUntil = DateTime.Now.AddSeconds(SecondsToLockQueueStatus);
+                    //}
 
                     PreviousStatus = m_status;
-					m_status = value;
-			
-					OnStatusChanged (new BuildStatusChangedEventArgs(this, PreviousStatus));
-				}
-			}
-		}
+                    m_status = value;
+
+                    OnStatusChanged(new BuildStatusChangedEventArgs(this, PreviousStatus));
+                }
+            }
+        }
 
         /// <summary>
         /// Gets the previous status.
         /// </summary>
         public BuildStatus PreviousStatus { get; private set; }
-		
-		public BuildStep LastRanStep { get; set; }
-		
-		public string LastChangeDescription { get; set; }
-		
-		public DateTime Date { get; set; }
-				
-		public bool IsSuccess {
-			get {
-				return m_status == BuildStatus.Success;
-			}
-		}
-		
-		public bool IsRunning {
-			get {
-				return m_status >= BuildStatus.Running;
-			}
-		}
-		
-		public bool IsQueued {
-			get {
-				return m_status == BuildStatus.Queued;
-			}
-		}
-		
-		public bool IsFailed
-		{
-			get {
-				return m_status <= BuildStatus.Canceled;
-			}
-		}
 
-		public float PercentageComplete { get; set; }
-		
-		/// <summary>
-		/// Gets or sets the user that triggered the build.
-		/// </summary>
-		public User TriggeredBy {
-			get {
-				return m_triggeredBy;
-			}
-		
-			set {
-				if (m_triggeredBy != value) {
-					var previousTriggeredBy = m_triggeredBy;
-					m_triggeredBy = value;
-			
-					if (m_triggeredBy != null) {
-						OnTriggeredByChanged (new BuildTriggeredByChangedEventArgs(this, previousTriggeredBy));
-					}
-				}
-			}
-		}
-		#endregion
-		
-		#region Methods
-		public override string ToString()
-		{
-			return String.Format ("{0} - {1}", Configuration.Project.Name, Configuration.Name);
-		}
-	
-		public int CompareTo (Build other)
-		{
-			var currentText = String.Format ("{0} - {1}", Configuration.Project.Name, Configuration.Name);
-			var otherText = String.Format ("{0} - {1}", other.Configuration.Project.Name, other.Configuration.Name);
-			return currentText.CompareTo (otherText);
-		}
-	
-		public object Clone ()
-		{
-			var c = (Build) this.MemberwiseClone ();
-			return c;
-		}
+        public BuildStep LastRanStep { get; set; }
 
-		private void OnStatusChanged(BuildStatusChangedEventArgs args)
-		{
-			if (StatusChanged != null) {
+        public string LastChangeDescription { get; set; }
+
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating whether the build is finish with success.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this instance is success; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsSuccess
+        {
+            get
+            {
+                return m_status == BuildStatus.Success;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the build is running.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this instance is running; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsRunning
+        {
+            get
+            {
+                return m_status >= BuildStatus.Running;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the build has been queued.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is queued; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsQueued
+        {
+            get
+            {
+                return m_status == BuildStatus.Queued;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the build has failed (Canceled | Error | Failed)
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is failed; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsFailed
+        {
+            get
+            {
+                return m_status <= BuildStatus.Canceled && m_status != BuildStatus.Unknown;
+            }
+        }
+
+        public float PercentageComplete { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user that triggered the build.
+        /// </summary>
+        public User TriggeredBy
+        {
+            get
+            {
+                return m_triggeredBy;
+            }
+
+            set
+            {
+                if (m_triggeredBy != value)
+                {
+                    var previousTriggeredBy = m_triggeredBy;
+                    m_triggeredBy = value;
+
+                    if (m_triggeredBy != null)
+                    {
+                        OnTriggeredByChanged(new BuildTriggeredByChangedEventArgs(this, previousTriggeredBy));
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Methods        
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return String.Format("{0} - {1}", Configuration.Project.Name, Configuration.Name);
+        }
+
+        /// <summary>
+        /// Compares to other build.
+        /// </summary>
+        /// <param name="other">The other.</param>
+        /// <returns></returns>
+        public int CompareTo(Build other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+
+            var currentText = String.Format("{0} - {1}", Configuration.Project.Name, Configuration.Name);
+            var otherText = String.Format("{0} - {1}", other.Configuration.Project.Name, other.Configuration.Name);
+
+            return currentText.CompareTo(otherText);
+        }
+
+        /// <summary>
+        /// Clones this instance.
+        /// </summary>
+        /// <returns>The clone.</returns>
+        public object Clone()
+        {
+            var c = (Build)this.MemberwiseClone();
+
+            return c;
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:StatusChanged" /> event.
+        /// </summary>
+        /// <param name="args">The <see cref="BuildStatusChangedEventArgs"/> instance containing the event data.</param>
+        private void OnStatusChanged(BuildStatusChangedEventArgs args)
+        {
+            if (StatusChanged != null)
+            {
                 var buildEvent = CallInterceptors((i, e) => i.OnStatusChanged(e));
 
                 if (!buildEvent.Canceled)
                 {
                     StatusChanged(this, args);
                 }
-			}
-		}
+            }
+        }
 
-		private void OnTriggeredByChanged(BuildTriggeredByChangedEventArgs args)
-		{
-			if (TriggeredByChanged != null) {
+        /// <summary>
+        /// Raises the <see cref="E:TriggeredByChanged" /> event.
+        /// </summary>
+        /// <param name="args">The <see cref="BuildTriggeredByChangedEventArgs"/> instance containing the event data.</param>
+        private void OnTriggeredByChanged(BuildTriggeredByChangedEventArgs args)
+        {
+            if (TriggeredByChanged != null)
+            {
                 var buildEvent = CallInterceptors((i, e) => i.OnTriggeredByChanged(e));
 
                 if (!buildEvent.Canceled)
                 {
                     TriggeredByChanged(this, args);
                 }
-			}
-		}
+            }
+        }
 
+        /// <summary>
+        /// Calls the interceptors.
+        /// </summary>
+        /// <param name="method">The method.</param>
+        /// <returns></returns>
         private BuildEvent CallInterceptors(Action<IBuildEventInterceptor, BuildEvent> method)
         {
             var buildEvent = new BuildEvent(this);
 
-            foreach(var interceptor in EventInterceptors)
+            foreach (var interceptor in EventInterceptors)
             {
                 method(interceptor, buildEvent);
             }
 
             return buildEvent;
         }
-		#endregion
-	}
+        #endregion
+    }
 }
-
