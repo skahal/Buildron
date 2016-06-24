edf7082216bb9448d54dcecde0ce69d4dcbbb9df
Diego Giacomelli <giacomelli@gmail.com>
2016-06-23 23:43:00 -0300

    Fix code format.

--- src/Buildron/Assets/_Assets/Scripts/Controllers/MainSceneController.cs
+++ src/Buildron/Assets/_Assets/Scripts/Controllers/MainSceneController.cs
@@ -12,31 +12,34 @@ using Buildron.Application;
 
 #endregion
 
-[AddComponentMenu("Buildron/Scenes/MainSceneController")]
+[AddComponentMenu ("Buildron/Scenes/MainSceneController")]
 public class MainSceneController : MonoBehaviour, IInitializable
 {
 	#region Fields
-	private Queue<GameObject> m_buildsToDeploy = new Queue<GameObject>();
+
+	private Queue<GameObject> m_buildsToDeploy = new Queue<GameObject> ();
 	private int m_deployedBuildsCount;
 	private bool m_isRefreshingBuilds;
 	private bool m_serverIsDown;
 
-    [Inject]
-    private IBuildService m_buildService;
+	[Inject]
+	private IBuildService m_buildService;
 
-    [Inject]
-    private IRemoteControlService m_remoteControlService;
+	[Inject]
+	private IRemoteControlService m_remoteControlService;
 
-    [Inject]
-    private ICIServerService m_ciServerService;
-    #endregion
+	[Inject]
+	private ICIServerService m_ciServerService;
 
-    #region Properties
-    public float DeployBuildSeconds = 0.5f;
+	#endregion
+
+	#region Properties
+
+	public float DeployBuildSeconds = 0.5f;
 	public int MaxDeployedBuilds = 32;
-	public Vector3 FirstColumnDeployPosition = new Vector3(-2.5f, 10, 0);
-	public Vector3 SecondColumnDeployPosition = new Vector3(2.5f, 10, 0);
-	public Vector3 HistoryColumnDeployPosition = new Vector3(0, 10, 20);
+	public Vector3 FirstColumnDeployPosition = new Vector3 (-2.5f, 10, 0);
+	public Vector3 SecondColumnDeployPosition = new Vector3 (2.5f, 10, 0);
+	public Vector3 HistoryColumnDeployPosition = new Vector3 (0, 10, 20);
 	public Text LastUpdateLabel;
 	public Text LogMessageLabel;
 	public Text ServerIPLabel;
@@ -48,11 +51,13 @@ public class MainSceneController : MonoBehaviour, IInitializable
 
 	[Inject]
 	public ServerMessagesListener ServerMessagesListener { get; set; }
+
 	#endregion
-	
+
 	#region Methods
-	
+
 	#region Initialize
+
 	private void Awake ()
 	{
 		SetLogMessage (string.Empty);
@@ -65,17 +70,17 @@ public class MainSceneController : MonoBehaviour, IInitializable
 			"OnBuildStopRequested");
 	}
 
-    public void Initialize()
-    {
-        var server = m_ciServerService.GetCIServer();
-        Title.text = server.Title;
-    }
+	public void Initialize ()
+	{
+		var server = m_ciServerService.GetCIServer ();
+		Title.text = server.Title;
+	}
 
-	public void StoreTitle()
+	public void StoreTitle ()
 	{
 		var server = m_ciServerService.GetCIServer ();
 		server.Title = Title.text;
-        m_ciServerService.SaveCIServer (server);
+		m_ciServerService.SaveCIServer (server);
 	}
 
 	private void OnCIServerReady ()
@@ -86,12 +91,12 @@ public class MainSceneController : MonoBehaviour, IInitializable
 		StartCoroutine (UpdateBuildsStatus ());	
 		StartCoroutine (DeployBuilds ());
 	}
-	
+
 	private void OnBuildRunRequested ()
 	{
 		ExecuteFocusedBuildCommand (m_buildService.RunBuild);
 	}
-	
+
 	private void OnBuildStopRequested ()
 	{
 		ExecuteFocusedBuildCommand (m_buildService.StopBuild);
@@ -101,52 +106,61 @@ public class MainSceneController : MonoBehaviour, IInitializable
 	{
 		var visibles = BuildGOService.GetVisibles ();
 		
-		if (visibles.Count == 1) {
+		if (visibles.Count == 1)
+		{
 			command (m_remoteControlService.GetConnectedRemoteControl (), visibles [0].GetComponent<BuildController> ().Model.Id);
 		}
 	}
-	
+
 	private void InitializeBuildService ()
 	{
 		SetLogMessage ("Contacting {0} server. Please, wait...", m_buildService.ServerName);
 
-        m_buildService.BuildFound += delegate(object sender, BuildFoundEventArgs e) {
-			if (m_buildService.BuildsCount == 1) {
+		m_buildService.BuildFound += delegate(object sender, BuildFoundEventArgs e)
+		{
+			if (m_buildService.BuildsCount == 1)
+			{
 				SetLogMessage ("");	
 			}
 		};
 
-        m_buildService.BuildsRefreshed += delegate {
+		m_buildService.BuildsRefreshed += delegate
+		{
 			m_isRefreshingBuilds = false;
 		};
 
-        m_ciServerService.CIServerStatusChanged += (e, args) => {
-			if (args.Server.Status == CIServerStatus.Down) {
+		m_ciServerService.CIServerStatusChanged += (e, args) =>
+		{
+			if (args.Server.Status == CIServerStatus.Down)
+			{
 				StartCoroutine (DelayServerIsDown ());
 				m_serverIsDown = true;
 				m_isRefreshingBuilds = false;
-			}
-			else {
+			} else
+			{
 				m_serverIsDown = false;
 
-				if (m_buildService.BuildsCount != 0) {
+				if (m_buildService.BuildsCount != 0)
+				{
 					SetLogMessage ("");	
 				}
 			}
 		};
 	}
-	
+
 	private IEnumerator DelayServerIsDown ()
 	{
 		// If server is already down, so there is another DelayServerIsDown in action.
-		if (!m_serverIsDown) {
-			yield return new WaitForSeconds(DelaySecondsUntilMarkAsServerIsDown);
+		if (!m_serverIsDown)
+		{
+			yield return new WaitForSeconds (DelaySecondsUntilMarkAsServerIsDown);
 			
 			// After the wait, the server still down?
-			if (m_serverIsDown) {
+			if (m_serverIsDown)
+			{
 				m_isRefreshingBuilds = false;
 				SetLogMessage ("{0} server is unavailable!", m_buildService.ServerName);
-				Messenger.Send("OnServerDown");
+				Messenger.Send ("OnServerDown");
 			}
 		}
 	}
@@ -155,30 +169,37 @@ public class MainSceneController : MonoBehaviour, IInitializable
 	{
 		ServerService.Initialize (ServerMessagesListener);
 	}
+
 	#endregion
-	
+
 	#region Updates
+
 	private void Update ()
 	{
-		if (Input.GetKey (KeyCode.Escape)) {
+		if (Input.GetKey (KeyCode.Escape))
+		{
 			Application.Quit ();
 		} 
 	}
-	
+
 	private IEnumerator UpdateBuildsStatus ()
 	{
 		var refreshSeconds = m_ciServerService.GetCIServer ().RefreshSeconds;
 		
-		while (true) {				
-			try {
-				if (!m_isRefreshingBuilds) {
+		while (true)
+		{				
+			try
+			{
+				if (!m_isRefreshingBuilds)
+				{
 					SetLastUpdateMessage ("Updating...");
 					m_isRefreshingBuilds = true;
-                    m_buildService.RefreshAllBuilds ();	
+					m_buildService.RefreshAllBuilds ();	
 					SetLastUpdateMessage ("Last update\n{0:HH:mm:ss}", System.DateTime.Now);
-					UpdateServerIP();
+					UpdateServerIP ();
 				}
-			} catch (System.Exception ex) {
+			} catch (System.Exception ex)
+			{
 				m_isRefreshingBuilds = false;
 				var baseEx = ex.GetBaseException ();
 				SetLogMessage ("ERROR: can't update. Please, check your connection with continuous integration server.\n{0}: {1}.", baseEx.GetType ().Name, baseEx.Message);
@@ -186,39 +207,44 @@ public class MainSceneController : MonoBehaviour, IInitializable
 			}
 			
 			SHLog.Debug ("Builds updated. Next update in {0} seconds.", refreshSeconds);
-			yield return new WaitForSeconds(refreshSeconds);
+			yield return new WaitForSeconds (refreshSeconds);
 		}
 	}
-	
+
 	private IEnumerator DeployBuilds ()
 	{
-		while (m_deployedBuildsCount < MaxDeployedBuilds) {
-			if (m_buildsToDeploy.Count > 0) {
+		while (m_deployedBuildsCount < MaxDeployedBuilds)
+		{
+			if (m_buildsToDeploy.Count > 0)
+			{
 				m_buildsToDeploy.Dequeue ().SetActive (true);
 				m_deployedBuildsCount++;
 			}
 		
-			yield return new WaitForSeconds(DeployBuildSeconds);	
+			yield return new WaitForSeconds (DeployBuildSeconds);	
 		}
 	}
+
 	#endregion
-	
+
 	#region Helpers
+
 	private void SetLastUpdateMessage (string msg, params object[] args)
 	{
 		LastUpdateLabel.text = System.String.Format (msg, args);
 	}
-	
+
 	private void SetLogMessage (string msg, params object[] args)
 	{
 		LogMessageLabel.text = System.String.Format (msg, args);
 	}
-	
-	private void UpdateServerIP()
+
+	private void UpdateServerIP ()
 	{
 		ServerIPLabel.text = Network.player.ipAddress;
 	}
+
 	#endregion
-	
+
 	#endregion
 }
