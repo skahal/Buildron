13164c0036f2b61ddd475c1014bd3f9745940733
Diego Giacomelli <giacomelli@gmail.com>
2016-06-25 17:28:45 -0300

    Improving ServerState CodeCoverage.

---
+++ src/Buildron/Assets/_Assets/Scripts/Domain.UnitTests/Editor/ServerStateTest.cs
@@ -0,0 +1,35 @@
+﻿using Buildron.Domain;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Buildron.Domain.UnitTests
+{
+    [Category("Buildron.Domain")]
+    public class ServerStateTest
+    {
+        [Test]
+		[Category("Unity")]
+		public void GetCameraPosition_State_Position()
+        {
+			var target = new ServerState ();
+			target.CameraPositionX = 1;
+			target.CameraPositionY = 2;
+			target.CameraPositionZ = 3;
+
+			var actual = target.GetCameraPosition ();
+			Assert.AreEqual (new Vector3 (1, 2, 3), actual);
+        }
+
+		[Test]
+		[Category("Unity")]
+		public void SetCameraPosition_Position_State()
+		{
+			var target = new ServerState ();
+			target.SetCameraPosition (new Vector3 (1, 2, 3));
+
+			Assert.AreEqual (1, target.CameraPositionX);
+			Assert.AreEqual (2, target.CameraPositionY);
+			Assert.AreEqual (3, target.CameraPositionZ);
+		}
+    }
+}
