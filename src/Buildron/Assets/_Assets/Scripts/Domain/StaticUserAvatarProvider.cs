0764fdad5f2e387f7088df267e2bb291161ca2ab
Diego Giacomelli <giacomelli@gmail.com>
2016-06-25 22:23:37 -0300

    Improving docs.

--- src/Buildron/Assets/_Assets/Scripts/Domain/StaticUserAvatarProvider.cs
+++ src/Buildron/Assets/_Assets/Scripts/Domain/StaticUserAvatarProvider.cs
@@ -1,49 +1,63 @@
-#region Usings
 using System;
 using System.Collections.Generic;
 using Buildron.Domain;
 using UnityEngine;
-#endregion
 
 namespace Buildron.Domain
 {
-    /// <summary>
-    /// An IUserAvatarProvider that use a static list of images to avatars.
-    /// </summary>
-    public class StaticUserAvatarProvider : IUserAvatarProvider
+	/// <summary>
+	/// An IUserAvatarProvider that use a static list of images to avatars.
+	/// </summary>
+	public class StaticUserAvatarProvider : IUserAvatarProvider
 	{
 		#region Fields
-		private Dictionary<string, Texture2D> m_photosByUsername= new Dictionary<string, Texture2D> ();
+		private Dictionary<string, Texture2D> m_photosByUsername = new Dictionary<string, Texture2D> ();
 		private Dictionary<UserKind, Texture2D> m_photosByKind = new Dictionary<UserKind, Texture2D> ();
 		#endregion
-		
-		#region IUserAvatarProvider implementation
+
+		#region Methods
+		/// <summary>
+		/// Gets the user photo.
+		/// </summary>
+		/// <param name="user">The user.</param>
+		/// <param name="photoReceived">The photo received callback.</param>
 		public void GetUserPhoto (User user, Action<Texture2D> photoReceived)
 		{
-			if (user == null) {
+			if (user == null)
+			{
 				return;
 			}
 			
-			if (m_photosByUsername.ContainsKey (user.UserName)) {
+			if (m_photosByUsername.ContainsKey (user.UserName))
+			{
 				photoReceived (m_photosByUsername [user.UserName]);
 			}
-			else if (m_photosByKind.ContainsKey (user.Kind)) {
+			else if (m_photosByKind.ContainsKey (user.Kind))
+			{
 				photoReceived (m_photosByKind [user.Kind]);
 			}
 
 			photoReceived (null);
 		}
-		#endregion
-		
-		#region Methods
-		public void AddPhoto(string userName, Texture2D photo)
+
+		/// <summary>
+		/// Adds the photo to the username.
+		/// </summary>
+		/// <param name="userName">User name.</param>
+		/// <param name="photo">Photo.</param>
+		public void AddPhoto (string userName, Texture2D photo)
 		{
-			m_photosByUsername[userName] = photo;
+			m_photosByUsername [userName] = photo;
 		}
 
-		public void AddPhoto(UserKind kind, Texture2D photo)
+		/// <summary>
+		/// Adds the photo to the user kiind.
+		/// </summary>
+		/// <param name="kind">Kind.</param>
+		/// <param name="photo">Photo.</param>
+		public void AddPhoto (UserKind kind, Texture2D photo)
 		{
-			m_photosByKind[kind] = photo;
+			m_photosByKind [kind] = photo;
 		}
 		#endregion
 	}
