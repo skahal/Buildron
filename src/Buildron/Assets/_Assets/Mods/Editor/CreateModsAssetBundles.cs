bed4be95d07375939ab3d20518c9e804d5426561
Diego Giacomelli <giacomelli@gmail.com>
2016-07-09 10:26:17 -0300

    Moved mods interfaces to external assembly Buildron.ModSdk to allow dynamically load and reference mods assemblies.

---
+++ src/Buildron/Assets/_Assets/Mods/Editor/CreateModsAssetBundles.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using System.Collections;
+
+public class CreateModsAssetBundles
+{
+
+	[MenuItem ("Buildron/Build mods")]
+	static void BuildAllAssetBundles ()
+	{
+        BuildPipeline.BuildAssetBundles ("../../Build/Mods", BuildAssetBundleOptions.None, BuildTarget.StandaloneOSXUniversal);
+       // BuildPipeline.BuildAssetBundles(@"..\..\Build\Mods", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+}
--- src/Buildron/Assets/_Assets/Scripts/Domain/Mods/ModLoader.cs
+++ src/Buildron/Assets/_Assets/Scripts/Domain/Mods/ModLoader.cs
@@ -41,56 +41,87 @@ namespace Buildron.Domain.Mods
 		{           
 			m_log.Debug ("Initialization started...");
 
-            ////var assetBundle = AssetBundle.LoadFromFile("/Users/giacomelli/Dropbox/Skahal/Apps/Buildron/build/Mods/consolemod");
+           // var assetBundle = AssetBundle.LoadFromFile("/Users/giacomelli/Dropbox/Skahal/Apps/Buildron/build/Mods/consolemod");
             //var assetBundle = AssetBundle.LoadFromFile(@"C:\Dropbox\Skahal\Apps\Buildron\build\Mods\consolemod");
 
-            //m_log.Debug("{0} Assets loaded.", assetBundle.GetAllAssetNames().Length);
+           // m_log.Debug("{0} Assets loaded.", assetBundle.GetAllAssetNames().Length);
 
 
-            //// VER ISSO
-            //// https://docs.unity3d.com/Manual/scriptsinassetbundles.html
-            //// Fazer CreateModsAssetBundles criar a versão TextAsset .txt de todos os .cs da pasta do mod. 
-            //// Depois carregar pelo reflection.
-            //var assets = assetBundle.LoadAllAssets<TextAsset>();
-            //Assembly modAssembly = null;
-
-            // http://answers.unity3d.com/questions/259569/how-to-compile-script-to-include-it-to-assetbundle.html
-            // https://docs.unity3d.com/Manual/UsingDLL.html
-
-            //foreach(var txt in assets)
-            //{
-            //    m_log.Debug("Loading code from asset {0}...", txt.name);
-
-            //    try {
-            //        modAssembly = System.Reflection.Assembly.Load(txt.bytes);
-            //        m_log.Debug("Assembly {0} loaded.", modAssembly.FullName);
-            //    }
-            //    catch(Exception ex)
-            //    {
-            //        m_log.Warning("Error loading code from asset: {0}", ex.Message);
-            //    }
-            //}
+            // VER ISSO
+            // https://docs.unity3d.com/Manual/scriptsinassetbundles.html
+            // Fazer CreateModsAssetBundles criar a versão TextAsset .txt de todos os .cs da pasta do mod. 
+            // Depois carregar pelo reflection.
+//            var assets = assetBundle.LoadAllAssets<TextAsset>();
+            Assembly modAssembly = null;
 
 
-
-            var modTypes = typeof(ModLoader).Assembly.GetTypes ().Where (t => !t.IsAbstract && typeof(IMod).IsAssignableFrom (t)).ToArray ();
-			m_log.Debug ("Found {0} mods", modTypes.Length);
-
-			foreach (var modType in modTypes) 
+			try 
 			{
-				m_log.Debug ("Loading mod from class '{0}'...", modType.FullName);
-				var mod = Activator.CreateInstance (modType) as IMod;
+				var mod = AppDomain.CurrentDomain.CreateInstanceFromAndUnwrap ("/Users/giacomelli/Dropbox/Console/test.dll", "Buildron.Mods.Console.Mod") as IMod;
 
-				if (mod == null) {
-					m_log.Warning ("Cannot create mod, there is no default constructor.");
-					continue;
-				}
-
-				m_log.Debug ("Initializing mod {0}...", mod.Name);
 				var context = new ModContext (mod, m_originalLog, m_buildService, m_ciServerService, m_remoteControlService, m_userService);
 				mod.Initialize (context);
-			}
 
+				m_log.Debug ("Mod created: {0}", mod.Name);
+			}
+			catch(Exception ex) {
+				m_log.Error (ex.Message);
+			}
+				
+           // http://answers.unity3d.com/questions/259569/how-to-compile-script-to-include-it-to-assetbundle.html
+           // https://docs.unity3d.com/Manual/UsingDLL.html
+
+//            foreach (var txt in assets)
+//            {
+//                m_log.Debug("Loading code from asset {0}...", txt.name);
+//
+//                try
+//                {
+//					modAssembly = System.Reflection.Assembly.Load(txt.bytes, );
+//                    m_log.Debug("Assembly {0} loaded.", modAssembly.FullName);
+//                }
+//                catch (Exception ex)
+//                {
+//                    m_log.Warning("Error loading code from asset: {0}", ex.Message);
+//                }
+//            }
+//
+//			m_log.Debug ("Types in bundle: ");
+//			Type[] bundleTypes;
+//
+//			try 
+//			{
+//			 	bundleTypes = modAssembly.GetTypes ();
+//			}
+//			catch(ReflectionTypeLoadException ex) {
+//				m_log.Warning ("Error loading types: {0}", ex.Message);
+//
+//				bundleTypes = ex.Types.Where (t => t != null).ToArray ();
+//			}
+//
+//			foreach (var bt in bundleTypes) {
+//				m_log.Debug ("\t {0}", bt.Name);
+//			}
+//
+//			//var modTypes = bundleTypes.Where (t => !t.IsAbstract && typeof(IMod).IsAssignableFrom (t)).ToArray ();
+//			var modTypes = bundleTypes.Where (t => !t.IsAbstract && t.GetInterface("IMod") != null).ToArray ();
+//			m_log.Debug ("Found {0} mods", modTypes.Length);
+
+//			foreach (var modType in modTypes) 
+//			{
+//				m_log.Debug ("Loading mod from class '{0}'...", modType.FullName);
+//				var mod = Activator.CreateInstance (modType) as IMod;
+//
+//				if (mod == null) {
+//					m_log.Warning ("Cannot create mod, there is no default constructor.");
+//					continue;
+//				}
+//
+//				m_log.Debug ("Initializing mod {0}...", mod.Name);
+//				var context = new ModContext (mod, m_originalLog, m_buildService, m_ciServerService, m_remoteControlService, m_userService);
+//				mod.Initialize (context);
+//			}
+//
 			m_log.Debug ("Initialization finished.");
 		}
 
