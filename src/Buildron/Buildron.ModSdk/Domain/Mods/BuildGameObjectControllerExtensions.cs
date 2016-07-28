83226b614020bb46133cd77227c9e727cd9af009
Diego Giacomelli <giacomelli@gmail.com>
2016-07-28 19:29:11 -0300

    Added Model to allow look for game object by model.

---
+++ src/Buildron/Buildron.ModSdk/Domain/Mods/BuildGameObjectControllerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using Buildron.Domain.Mods;
+using System.Collections.Generic;
+using Buildron.Domain.Builds;
+
+public static class BuildGameObjectControllerExtensions
+{	
+    public static GameObject GetGameObject (this IBuildController[] builds, IBuild build)
+    {
+        var controller = builds.FirstOrDefault(b => b.Model == build);
+
+        return controller == null ? null : controller.gameObject;
+    }
+}
--- src/Buildron/Buildron.ModSdk/Domain/Mods/IBuildController.cs
+++ src/Buildron/Buildron.ModSdk/Domain/Mods/IBuildController.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using Buildron.Domain.Builds;
 using UnityEngine;
 
 namespace Buildron.Domain.Mods
 {
 	public interface IBuildController : IGameObjectController
 	{
-		
+		IBuild Model { get;  }
 	}
 }
