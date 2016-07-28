e9367af3193e4e37ada410c28cf6e94111fb8bac
Diego Giacomelli <giacomelli@gmail.com>
2016-07-27 23:55:05 -0300

    Moving extensions from BuildGOService.

--- src/Buildron/Buildron.ModSdk/Domain/Mods/GameObjectControllerExtensions.cs
+++ src/Buildron/Buildron.ModSdk/Domain/Mods/GameObjectControllerExtensions.cs
@@ -86,5 +86,64 @@ public static class GameObjectControllerExtensions
 	{
 		return builds.Where (b => b.Rigidbody != null && Mathf.Abs (b.Rigidbody.velocity.y) <= 0.1f).ToArray ();
 	}
-}
 
+	/// <summary>
+	/// Verify if all builds physics are sleeping.
+	/// </summary>
+	/// <remarks>
+	/// Works well with the value of "Sleep Threshold" in the "Project settings\Physics" as "0.05".
+	/// </remarks>
+	/// <returns>True if all builds are sleeping.</returns>
+	public static bool AreAllSleeping(this IGameObjectController[] builds)
+	{
+		return builds.All(b => b.Rigidbody.IsSleeping());
+	}
+
+	/// <summary>
+	/// Issue #9: https://github.com/skahal/Buildron/issues/9
+	/// If a build was removed, maybe there are space between builds totems and some can be sleeping.
+	/// Wake everyone!
+	/// </summary>
+	public static void WakeUpSleepingBuilds(this IGameObjectController[] builds)
+	{      
+		foreach (var build in builds)
+		{
+			var rb = build.Rigidbody;
+
+			if (rb.IsSleeping())
+			{
+				rb.WakeUp();
+			}
+		}
+	}
+
+	/// <summary>
+	/// Freezes all builds game objects
+	/// </summary>
+	public static void FreezeAll(this IGameObjectController[] builds)
+	{
+		foreach (var build in builds)
+		{
+			var rb = build.Rigidbody;
+			rb.isKinematic = true;
+
+			// Allows build game object be moved on X and Y (sorting swap effect).
+			rb.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
+		}
+	}
+
+	/// <summary>
+	/// Unfreezes all builds game objects
+	/// </summary>
+	public static void UnfreezeAll(this IGameObjectController[] builds)
+	{
+		foreach (var build in builds)
+		{
+			var rb = build.Rigidbody;
+			rb.isKinematic = false;
+
+			// Allows build game object be moved only Y (explosion effects).
+			rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
+		}
+	}
+}
