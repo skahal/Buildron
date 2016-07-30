5310733696f93fdd4a87c0992d10db2f3b32691d
Diego Giacomelli <giacomelli@gmail.com>
2016-07-30 19:49:19 -0300

    Extension to create a game object directly from an prefab name,

--- src/Buildron/Buildron.ModSdk/Domain/Mods/GameObjectsProxyExtensions.cs
+++ src/Buildron/Buildron.ModSdk/Domain/Mods/GameObjectsProxyExtensions.cs
@@ -2,46 +2,46 @@
 using UnityEngine;
 using Buildron.Domain.Mods;
 
-	public static class GameObjectsProxyExtensions
+public static class GameObjectsProxyExtensions
+{
+	public static TComponent Create<TComponent> (this IGameObjectsProxy proxy, string name = null, Transform parent = null) 
+			where TComponent : Component
 	{
-		public static TComponent Create<TComponent> (this IGameObjectsProxy proxy, string name = null, Transform parent = null) 
-			where TComponent : Component
-		{
-			return proxy.Create<TComponent>(name, (go) => {
-				go.transform.parent = parent;
-			});
-		}
-
-		public static TComponent Create<TComponent> (this IGameObjectsProxy proxy, string name, Vector3 position) 
-			where TComponent : Component
-		{
-			return proxy.Create<TComponent>(name, (go) => {
-				go.transform.position = position;
-			});
-		}
-
-		public static TComponent Create<TComponent> (this IGameObjectsProxy proxy, Vector3 position) 
-			where TComponent : Component
-		{
-			return proxy.Create<TComponent>(null, (go) => {
-				go.transform.position = position;
-			});
-		}
-
-		public static GameObject Create(this IGameObjectsProxy proxy, UnityEngine.Object prefab, Transform parent = null)
-		{
-			var go = proxy.Create (prefab);
+		return proxy.Create<TComponent> (name, (go) => {
 			go.transform.parent = parent;
-
-			return go;
-		}
-
-		public static GameObject Create(this IGameObjectsProxy proxy, string name, Transform parent = null)
-		{
-			var go = proxy.Create (name);
-			go.transform.parent = parent;
-
-			return go;
-		}
+		});
 	}
+
+	public static TComponent Create<TComponent> (this IGameObjectsProxy proxy, string name, Vector3 position) 
+			where TComponent : Component
+	{
+		return proxy.Create<TComponent> (name, (go) => {
+			go.transform.position = position;
+		});
+	}
+
+	public static TComponent Create<TComponent> (this IGameObjectsProxy proxy, Vector3 position) 
+			where TComponent : Component
+	{
+		return proxy.Create<TComponent> (null, (go) => {
+			go.transform.position = position;
+		});
+	}
+
+	public static GameObject Create (this IGameObjectsProxy proxy, UnityEngine.Object prefab, Transform parent = null)
+	{
+		var go = proxy.Create (prefab);
+		go.transform.parent = parent;
+
+		return go;
+	}
+
+	public static GameObject Create (this IGameObjectsProxy proxy, string name, Transform parent = null)
+	{
+		var go = proxy.Create (name);
+		go.transform.parent = parent;
+
+		return go;
+	}
+}
 
---
+++ src/Buildron/Buildron.ModSdk/Domain/Mods/ModContextExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using Buildron.Domain.Mods;
+
+public static class ModContextExtensions
+{
+	public static GameObject CreateGameObjectFromPrefab (this IModContext context, string prefabName)
+	{
+		var prefab = context.Assets.Load (prefabName);
+
+		return context.GameObjects.Create (prefab);
+	}
+}
