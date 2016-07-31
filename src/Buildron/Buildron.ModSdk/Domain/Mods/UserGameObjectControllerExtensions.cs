eaf4dfcb704cac1bd02e075e6e7d399eb90825b7
Diego Giacomelli <giacomelli@gmail.com>
2016-07-31 14:56:51 -0300

    Extension method to find user game object by username.

---
+++ src/Buildron/Buildron.ModSdk/Domain/Mods/UserGameObjectControllerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using Buildron.Domain.Mods;
+
+public static class UserGameObjectControllerExtensions
+{	
+	public static GameObject GetByUsername (this IUserController[] users, string userName)
+    {
+		var controller = users.FirstOrDefault (u => u.Model.UserName.Equals (userName, StringComparison.OrdinalIgnoreCase));
+   
+		return controller == null ? null : controller.gameObject;
+	}
+}
